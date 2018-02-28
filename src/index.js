ea85c1d1305d9738d0c564b10829da48dfaf954a
redgektor <redgektor11@gmail.com>
2018-02-28 20:25:17 +0300

    advanced-zeros: 79pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+var subBase = base, subNumber = number, result = 0;
+	for (var i = 2; i <= base; i++) {
+		if (subBase % i == 0) {
+			while (subBase % i == 0) {
+				subBase = (subBase - subBase%i)/i;
+			}
+			
+			subNumber = number;
+			result = 0;
+			while (subNumber/i > 0) {
+				result = result + ((subNumber - subNumber%i) / i);
+				subNumber = (subNumber - subNumber%i) / i;
+			}
+			if (subNumber > result) {
+				subNumber = result;
+			}
+		}
+	}
+	return Math.floor(result);
 }
